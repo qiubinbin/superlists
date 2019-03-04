c04d99e7a5c6816d95708a76210af6bba0b6bf15
qiubinbin <qiubin780061109@163.com>
2019-03-05 00:55:03 +0800

    修改功能测试部分

--- functional_test.py
+++ functional_test.py
@@ -1,22 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitor(unittest.TestCase):
     def setUp(self):
         """执行设置操作"""
-        self.brower = webdriver.Chrome()
-        self.brower.implicitly_wait(3)
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         """在运行测试后，执行清除操作"""
-        self.brower.quit()
+        self.browser.quit()
 
     def test_can_start_a_list_and_interactive_it_later(self):
         """测试部分必须test开头"""
-        self.brower.get('http://127.0.0.1')
-        self.assertIn('To-Do', self.brower.title)  # 断言
-        self.fail('Finsih the test!')
+        # 头部和标题都包括'To-Do'
+        self.browser.get('http://localhost:8000')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # 邀请他输入待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        # 她在文本框输入了做鱼饵
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows))
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        reponse = home_page(request)
+        self.assertTrue(reponse.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', reponse.content)
+        self.assertTrue(reponse.content.endswith(b'</html>'))
