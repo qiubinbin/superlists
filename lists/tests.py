7e21fa5e8ff8ed731d4ca2cbdc3e4b787b73a876
qiubinbin <qiubin780061109@163.com>
2019-03-05 01:19:38 +0800

    增加render_to_string

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -12,6 +13,8 @@ class HomePageTest(TestCase):
     def test_home_page_return_correct_html(self):
         request = HttpRequest()
         reponse = home_page(request)
+        expected_html = render_to_string('home.html')
         self.assertTrue(reponse.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', reponse.content)
         self.assertTrue(reponse.content.endswith(b'</html>'))
+        self.assertEqual(reponse.content.decode(), expected_html)
